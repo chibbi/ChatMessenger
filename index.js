874b5c70822d173a9d90b3e5b79c614eed822eb2
chibbi <chehab.djibril@gmail.com>
2020-11-16 01:26:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,197 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var fs = require("fs");
+var readLastLine = require('read-last-line');
+
+var port = 3000;
+var dirname = "/Path/to/your/GithubRepos/ChatMessenger/";
+var filename = dirname + "/data/messages.log";
+var servername = "SERVER ::: ";
+var islogging = true;
+
+var usernumber = 0;
+var usernamedb = [];
+var useripdb = [];
+var bannedipdb = [];
+
+app.get("/", (req, res) => {
+    res.sendFile(dirname + "static/index.html");
+});
+app.get("/css/default.css", (req, res) => {
+    res.sendFile(dirname + "static/css/default.css");
+});
+app.get("/js/default.js", (req, res) => {
+    res.sendFile(dirname + "static/js/default.js");
+});
+
+io.on("connection", (socket) => {
+    printlog("New user connected (" + socket.handshake.address + ")");
+
+    addIp(socket.handshake.address);
+    usernumber++;
+    console.log(usernumber);
+
+    socket.broadcast.emit("chat message", servername + "Someone joined");
+
+    readLastLine.read(dirname + "data/messages.log", 25).then(function (lines) {
+        var lastM = lines.split("'");
+        for (var i = 0; i <= lastM.length; i++) {
+            if (i % 2) {
+                socket.emit("chat message", lastM[i]);
+            }
+        }
+    }).catch(function (err) {
+        console.log(err.message);
+    });;
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+        io.emit("chat message", servername + "Someone disconnected");
+        usernumber--;
+        console.log(usernumber);
+    });
+    socket.on("chat message", (msg) => {
+        var sendable = "yes";
+        if (isBanned(socket.handshake.address)) { sendable = "banned" };
+        if (hasBadLetters(msg)) { sendable = "bad msg" };
+        var temp = isCommand(msg, socket.handshake.address, sendable);
+        var output = temp[1];
+        sendable = temp[0];
+        switch (sendable) {
+            case "yes":
+                msg = usernamedb[socket.handshake.address] + " wrote: " + msg;
+                printlog(msg);
+                io.emit("chat message", msg);
+                break;
+            case "temp":
+                msg = usernamedb[socket.handshake.address] + " wrote: " + msg;
+                console.log(msg);
+                io.emit("chat message", msg);
+                break;
+            case "does":
+                msg = usernamedb[socket.handshake.address] + " " + msg;
+                console.log(addTimeStamp(msg));
+                io.emit("chat message", msg);
+                break;
+            case "command":
+                msg = usernamedb[socket.handshake.address] + " wrote: " + msg;
+                printlog(msg);
+                msg = usernamedb[socket.handshake.address] + " got: " + output;
+                io.emit("chat message", msg);
+                break;
+            case "banned":
+                printlog(
+                    usernamedb[socket.handshake.address] +
+                    " wrote: '" +
+                    msg +
+                    "' IP BANNED"
+                );
+                socket.emit(
+                    "chat message",
+                    servername + "you are BANNED"
+                );
+                break;
+            case "bad msg":
+                usernamedb[socket.handshake.address] + " wrote: '" + msg + "' DENIED"
+                socket.emit(
+                    "chat message",
+                    servername + "you are NOT ALLOWED to SEND THAT"
+                );
+                break;
+        }
+    });
+    socket.on("callusernum", () => {
+        console.log(socket.handshake.address + " pressed getUsers");
+        var i = 1;
+        useripdb.forEach((ip) => {
+            var name = usernamedb[ip];
+            io.emit("chat message", servername + "user [" + i + "]: " + name);
+            i++;
+        });
+    });
+    socket.on("changename", (msg) => {
+        printlog(socket.handshake.address + " changes Names to: " + msg);
+        usernamedb[socket.handshake.address] = msg;
+        socket.emit("chat message", "changed username to: " + msg);
+    });
+});
+
+http.listen(port, () => {
+    console.log("listening on *:" + port);
+});
+
+function addIp(addr) {
+    var exists = false;
+    useripdb.forEach((ip) => {
+        if (ip == addr) {
+            exists = true;
+        }
+    });
+    if (!exists) {
+        useripdb.push(addr);
+    }
+}
+
+function isCommand(msg, ip, sendable) {
+    if (msg.includes("!temp")) {
+        return ["temp", msg];
+    }
+    if (msg.includes("!d")) {
+        return ["does", msg];
+    }
+    switch (msg) {
+        case "!getIP":
+            return ["command", ip];
+            break;
+    }
+    return [sendable, "No Command"];
+}
+
+function hasBadLetters(msg) {
+    if (
+        msg.includes("SERVER") ||
+        msg.replace(/^\s+|\s+$/g, "") == "" ||
+        msg == "test"
+    ) {
+        return true;
+    }
+}
+
+function isBanned(addr) {
+    for (var i = 0; i <= bannedipdb.length; i++) {
+        if (bannedipdb[i] == addr) {
+            return true;
+        }
+    }
+}
+
+function addTimeStamp(text) {
+    var curdate = new Date();
+    text =
+        ("0" + curdate.getDate()).slice(-2) +
+        "-" +
+        ("0" + (curdate.getMonth() + 1)).slice(-2) +
+        "-" +
+        curdate.getFullYear() +
+        "_" +
+        curdate.getHours() +
+        ":" +
+        curdate.getMinutes() +
+        ":" +
+        curdate.getSeconds() +
+        " :: " +
+        text;
+    return text;
+}
+
+function printlog(text) {
+    var curdate = new Date();
+    text = addTimeStamp(text);
+    if (islogging) {
+        fs.appendFile(filename, "'" + text + "'\n", "utf8", (err) => {
+            if (err) throw err;
+        });
+    }
+    console.log(text);
+}
---
+++ static/js/default.js
@@ -0,0 +1,60 @@
+var username;
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function getUsername() {
+  var person = getCookie("username");
+  if (person == null || person == "") {
+    setUsername
+  }
+  username = person;
+}
+
+function setUsername() {
+  var person = window.prompt("pls enter your Username", "Randy");
+  document.cookie =
+    "username=" + person + ";expires=Mon, 04 Jul 2028 22:44:25 UTC";
+  if (person == null || person == "") {
+    setUsername();
+  }
+  username = person;
+}
+
+$(function () {
+  var socket = io();
+  socket.emit("changename", username);
+  $("form").submit(function (e) {
+    e.preventDefault();
+    socket.emit("chat message", $("#m").val());
+    $("#m").val("");
+    return false;
+  });
+  $("#getUs").click(function () {
+    socket.emit("callusernum");
+  });
+  $("#setName").click(function () {
+    setUsername();
+    socket.emit("changename", username);
+  });
+  socket.on("chat message", function (msg) {
+    $("#messages").append($("<li>").text(msg));
+    var elem = document.getElementById("divmess");
+    elem.scrollTop = elem.scrollHeight;
+  });
+});
+
+getUsername();
